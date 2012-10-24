517be5003b9a89032edd48c710973f77ad6a9efb
Dave Stevens <dave@spindle.com>
2012-10-24 19:57:36 -0400

    Add additional test showing that issue is related to mock method parameters that have a type parameter

--- src/test/scala/com/bifflabs/MockSuite.scala
+++ src/test/scala/com/bifflabs/MockSuite.scala
@@ -7,22 +7,44 @@ import org.mockito.Mockito.when
 
 trait MockingBird {
 
-  def sing(input: List[String]): String
+  //Assertion does not fail when input does not have Type param
+  def sing(input: Set[String]): String
+
+}
+
+trait MockingBirdWithStrings {
+
+  //Assertion does not fail when input does not have Type param
+  def sing(input: String): String
 
 }
 
 class MockSuite extends FunSuite with ShouldMatchers with MockitoSugar {
 
-  val iWannaRock = List("I wanna Rock")
-  val beatingDeadHorse = List("Sometimes I feel like I'm beating a dead horse")
+  //Make sure it's not using object equals to match
+  val (iWannaRock, rock) = (Set("I wanna Rock"), "Rock!")
+  val (beatingDeadHorse, bringinMeDown) = (Set("Sometimes I feel like I'm beating a dead horse"), "An I don't know why you'd be bringing me down")
 
   val mockMockingBird = mock[MockingBird]
-  when(mockMockingBird.sing(beatingDeadHorse)).thenReturn("An I don't know why you'd be bringing me down")
-  when(mockMockingBird.sing(iWannaRock)).thenReturn("Rock!")
+  when(mockMockingBird.sing(iWannaRock)).thenReturn(rock)
 
-  test("A mock should sing") {
-    assert(mockMockingBird.sing(beatingDeadHorse) === "An I don't know why you'd be bringing me down")
-    assert(mockMockingBird.sing(iWannaRock) === "Rock!")
+  //Appears to return this whenever any Set is passed to sing
+  when(mockMockingBird.sing(beatingDeadHorse)).thenReturn(bringinMeDown)
+
+  test("A mock should match on parameter but isn't") {
+    assert(mockMockingBird.sing(Set.empty) === bringinMeDown)
+    assert(mockMockingBird.sing(beatingDeadHorse) === bringinMeDown)
+    assert(mockMockingBird.sing(iWannaRock) === bringinMeDown)
+  }
+
+  val mockMockingBirdWithStrings = mock[MockingBirdWithStrings]
+  when(mockMockingBirdWithStrings.sing("I wanna Rock")).thenReturn(rock)
+  when(mockMockingBirdWithStrings.sing("Sometimes I feel like I'm beating a dead horse")).thenReturn(bringinMeDown)
+
+  test("A mock should match on parameter and does type of parameter has no type params") {
+    assert(mockMockingBirdWithStrings.sing("") === null)
+    assert(mockMockingBirdWithStrings.sing("I wanna Rock") === rock)
+    assert(mockMockingBirdWithStrings.sing("Sometimes I feel like I'm beating a dead horse") === bringinMeDown)
   }
 
 }
