fcb9adac60601304e739880543df9c4ad5915548
Dave Stevens <dave@spindle.com>
2012-10-31 23:07:54 -0400

    Clean up test a bit for stackoverflow post

--- project/ScalatestandspecscoexistingBuild.scala
+++ project/ScalatestandspecscoexistingBuild.scala
@@ -8,19 +8,19 @@ object ScalatestandspecscoexistingBuild extends Build {
   lazy val mockito = "org.mockito" % "mockito-core" % "1.9.0" % "test"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "1.8" % "test"
 
-  lazy val scalatestandspecscoexisting = Project(
-    id = "scalatest-and-specs-coexisting",
-    base = file("."),
-    settings = Project.defaultSettings ++
-      GraphPlugin.graphSettings ++
-      Seq(
-      name := "Scalatest-And-Specs-Coexisting",
-      organization := "com.bifflabs",
-      version := "0.1",
-      scalaVersion := "2.9.2",
-//      libraryDependencies ++= Seq(scalaTest, mockito)   //Tests Pass
-//      libraryDependencies ++= Seq(scalaTest, mockito, specs2)  //Tests Pass?!?
-      libraryDependencies ++= Seq(scalaTest, specs2, mockito)  //Tests Fail
+    lazy val scalatestandspecscoexisting = Project(
+      id = "scalatest-and-specs-coexisting",
+      base = file("."),
+      settings = Project.defaultSettings ++
+        GraphPlugin.graphSettings ++
+        Seq(
+        name := "Scalatest-And-Specs-Coexisting",
+        organization := "com.bifflabs",
+        version := "0.1",
+        scalaVersion := "2.9.2",
+  //      libraryDependencies ++= Seq(scalaTest, mockito)   //Tests Pass
+  //      libraryDependencies ++= Seq(scalaTest, mockito, specs2)  //Tests Pass?!?
+        libraryDependencies ++= Seq(scalaTest, specs2, mockito)  //Tests Fail
+      )
     )
-  )
 }
--- src/test/scala/com/bifflabs/MockSuite.scala
+++ src/test/scala/com/bifflabs/MockSuite.scala
@@ -1,50 +1,51 @@
 package com.bifflabs
 
-import org.scalatest.FunSuite
-import org.scalatest.mock.MockitoSugar
-import org.scalatest.matchers.ShouldMatchers
-import org.mockito.Mockito.when
+    import org.scalatest.FunSuite
+    import org.scalatest.mock.MockitoSugar
+    import org.mockito.Mockito.when
 
-trait MockingBird {
+    trait MockingBird {
+      //Assertion does not fail when input does not have Type param
+      def sing(input: Set[String]): String
+    }
 
-  //Assertion does not fail when input does not have Type param
-  def sing(input: Set[String]): String
+    trait MockingBirdWithStrings {
+      //Assertion does not fail when input does not have Type param
+      def sing(input: String): String
+    }
 
-}
+    class MockSuite extends FunSuite with MockitoSugar {
 
-trait MockingBirdWithStrings {
+      // Expected parameter must have ≥ 1 type param
+      // for described behavior to be expressed, hence the Set
+      val iWannaRock = Set("I wanna Rock")
+      val rock = "Rock!"
 
-  //Assertion does not fail when input does not have Type param
-  def sing(input: String): String
+      val wereNotGonnaTakeIt = Set("We're not gonna take it")
+      val No = "No! We ain't gonna take it"
 
-}
+      test("A mock should match on parameter but isn't") {
 
-class MockSuite extends FunSuite with ShouldMatchers with MockitoSugar {
+        val mockMockingBird = mock[MockingBird]
+        when(mockMockingBird.sing(iWannaRock)).thenReturn(rock)
+        //Appears to return this whenever any Set is passed to sing
+        when(mockMockingBird.sing(wereNotGonnaTakeIt)).thenReturn(No)
 
-  //Make sure it's not using object equals to match
-  val (iWannaRock, rock) = (Set("I wanna Rock"), "Rock!")
-  val (beatingDeadHorse, bringinMeDown) = (Set("Sometimes I feel like I'm beating a dead horse"), "An I don't know why you'd be bringing me down")
-
-  val mockMockingBird = mock[MockingBird]
-  when(mockMockingBird.sing(iWannaRock)).thenReturn(rock)
-
-  //Appears to return this whenever any Set is passed to sing
-  when(mockMockingBird.sing(beatingDeadHorse)).thenReturn(bringinMeDown)
-
-  test("A mock should match on parameter but isn't") {
-    assert(mockMockingBird.sing(Set.empty) === null)
-    assert(mockMockingBird.sing(beatingDeadHorse) === bringinMeDown)
-    assert(mockMockingBird.sing(iWannaRock) === rock)
-  }
-
-  val mockMockingBirdWithStrings = mock[MockingBirdWithStrings]
-  when(mockMockingBirdWithStrings.sing("I wanna Rock")).thenReturn(rock)
-  when(mockMockingBirdWithStrings.sing("Sometimes I feel like I'm beating a dead horse")).thenReturn(bringinMeDown)
+        // Succeeds because it was last
+        assert(mockMockingBird.sing(wereNotGonnaTakeIt) === No)
+        // Fails because the mock returns value in bringinMeDown
+        assert(mockMockingBird.sing(iWannaRock) === rock)
+      }
 
   test("A mock should match on parameter and does type of parameter has no type params") {
+
+    val mockMockingBirdWithStrings = mock[MockingBirdWithStrings]
+    when(mockMockingBirdWithStrings.sing(iWannaRock.head)).thenReturn(rock)
+    when(mockMockingBirdWithStrings.sing(wereNotGonnaTakeIt.head)).thenReturn(No)
+
     assert(mockMockingBirdWithStrings.sing("") === null)
-    assert(mockMockingBirdWithStrings.sing("I wanna Rock") === rock)
-    assert(mockMockingBirdWithStrings.sing("Sometimes I feel like I'm beating a dead horse") === bringinMeDown)
+    assert(mockMockingBirdWithStrings.sing(iWannaRock.head) === rock)
+    assert(mockMockingBirdWithStrings.sing(wereNotGonnaTakeIt.head) === No)
   }
 
 }
